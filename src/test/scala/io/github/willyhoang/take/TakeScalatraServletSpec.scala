70eae20bf18e856ba794654a070eff8981e887e8
Willy Hoang <willy.hoang@blueapron.com>
2016-06-30 02:10:02 -0400

    Update test to use new TakeServlet constructor
    Also included fixes to make mobile experience more pleasant.

--- src/test/scala/io/github/willyhoang/take/TakeScalatraServletSpec.scala
+++ src/test/scala/io/github/willyhoang/take/TakeScalatraServletSpec.scala
@@ -1,5 +1,6 @@
 package io.github.willyhoang.take
 
+import akka.actor.{Props, ActorSystem}
 import org.scalatra.test.specs2._
 
 // For more on Specs2, see http://etorreborre.github.com/specs2/guide/org.specs2.guide.QuickStart.html
@@ -8,7 +9,10 @@ class TakeScalatraServletSpec extends ScalatraSpec { def is =
     "should return status 200"                  ! root200^
                                                 end
 
-  addServlet(classOf[TakeScalatraServlet], "/*")
+  val system = ActorSystem()
+  val myActor = system.actorOf(Props(new ClassesRetrieverActor(system)))
+  val servlet = new TakeScalatraServlet(system, myActor)
+  addServlet(servlet, "/*")
 
   def root200 = get("/") {
     status must_== 200
