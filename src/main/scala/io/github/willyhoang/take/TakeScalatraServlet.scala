426094a0ffbfa0589d0ebd22f71c82ac5f838ee2
Willy Hoang <willy.hoang@blueapron.com>
2016-07-07 09:56:25 -0400

    Add support for CORS in Scalatra servlet
    This enables us to make GET requests from other domains (github.io).

--- src/main/scala/io/github/willyhoang/take/TakeScalatraServlet.scala
+++ src/main/scala/io/github/willyhoang/take/TakeScalatraServlet.scala
@@ -3,19 +3,25 @@ package io.github.willyhoang.take
 import akka.actor.{ActorRef, ActorSystem}
 import akka.util.Timeout
 import org.scalatra.guavaCache.GuavaCache
-import org.scalatra.{AsyncResult, FutureSupport}
+import org.scalatra.{CorsSupport, AsyncResult, FutureSupport}
 
 import scala.concurrent.ExecutionContext
 import scala.concurrent.duration._
 
 class TakeScalatraServlet(system: ActorSystem, classesRetrieverActor: ActorRef)
-  extends TakeStack with FutureSupport {
+  extends TakeStack with FutureSupport with CorsSupport {
 
   implicit val cacheBackend = GuavaCache
   implicit def executor: ExecutionContext = system.dispatcher
   implicit val defaultTimeout: Timeout = Timeout(60 seconds)
 
   import _root_.akka.pattern.ask
+
+  options("/classes/:date") {
+    response.setHeader("Access-Control-Allow-Headers",
+      request.getHeader("Access-Control-Request-Headers"))
+    response.setHeader("Access-Control-Allow-Origin", "*")
+  }
 
   get("/") {
     contentType = "text/html"
