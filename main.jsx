dac88cccd936b51c8fa5521a759fba253b5ff4a0
Willy Hoang <willy.hoang@blueapron.com>
2016-07-10 00:58:29 -0400

    Add datepicker to choose classes by day
    All state lives in DanceClassesBody. Date state comes from
    ClassesDatePicker. Classes data state is updated from state date in
    DanceClassesBody and passed to DanceClassesTable.

--- main.jsx
+++ main.jsx
@@ -9,6 +9,9 @@ var NavDropdown= require('react-bootstrap').NavDropdown;
 var MenuItem = require('react-bootstrap').MenuItem;
 var Table = require('react-bootstrap').Table;
 var Panel = require('react-bootstrap').Panel;
+var DatePicker = require("react-bootstrap-date-picker");
+var FormGroup = require('react-bootstrap').FormGroup;
+var FormControl = require('react-bootstrap').FormControl;
 
 var Container = React.createClass({
   render: function() {
@@ -20,34 +23,62 @@ var Container = React.createClass({
   }
 });
 
-const navBarInstance = (
-  <Navbar fluid={true} bsStyle="inverse">
-    <Navbar.Header>
-      <Navbar.Brand>
-        <a href="#">React-Bootstrap</a>
-      </Navbar.Brand>
-      <Navbar.Toggle />
-    </Navbar.Header>
-    <Navbar.Collapse>
-      <Nav>
-        <NavItem eventKey={1} href="#">Link</NavItem>
-        <NavItem eventKey={2} href="#">Link</NavItem>
-        <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-          <MenuItem eventKey={3.1}>Action</MenuItem>
-          <MenuItem eventKey={3.2}>Another action</MenuItem>
-          <MenuItem eventKey={3.3}>Something else here</MenuItem>
-          <MenuItem divider />
-          <MenuItem eventKey={3.3}>Separated link</MenuItem>
-        </NavDropdown>
-      </Nav>
-      <Nav pullRight>
-        <NavItem eventKey={1} href="#">Link Right</NavItem>
-        <NavItem eventKey={2} href="#">Link Right</NavItem>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
+var ClassesDatePicker = React.createClass({
+  getInitialState: function(){
+    return {
+      date: this.props.initialDate
+    };
+  },
+  handleChange: function(value) {
+    // value is an ISO String.
+    this.setState({
+      date: value
+    });
+    this.props.callbackParent(value);
+  },
+  render: function(){
+    return <FormGroup>
+      <DatePicker value={this.state.date} onChange={this.handleChange} />
+    </FormGroup>;
+  }
+});
 
-);
+var ClassesNavBar = React.createClass({
+  getInitialState: function(){
+    return {
+      date: this.props.initialDate
+    };
+  },
+  onChildChange: function(updatedDate) {
+    console.log("setting state for classes nav bar")
+    this.setState({ date: updatedDate });
+    console.log("calling parent (danceclassesbody) on child changed method")
+    this.props.callbackParent(updatedDate);
+  },
+  render: function() {
+    return (
+      <Navbar fluid={true} bsStyle="inverse">
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="#">React-Bootstrap</a>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav>
+            <Navbar.Form pullLeft>
+                <ClassesDatePicker initialDate={this.state.date} callbackParent={this.onChildChange} />
+            </Navbar.Form>
+          </Nav>
+          <Nav pullRight>
+            <NavItem eventKey={1} href="#">Link Right</NavItem>
+            <NavItem eventKey={2} href="#">Link Right</NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
+});
 
 /**
  * Converts a JSON time into a human readable string.
@@ -88,26 +119,13 @@ var DanceClass = React.createClass({
   }
 });
 
+function formatDateForUrl(isoDate) {
+  return isoDate.substring(0, 10);
+}
 
 var DanceClassesTable = React.createClass({
-  getInitialState: function() {
-    return {data: []};
-  },
-  componentDidMount: function() {
-    $.ajax({
-      url: this.props.url,
-      dataType: 'json',
-      cache: false,
-      success: function(data) {
-        this.setState({data: data});
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.error(this.props.url, status, err.toString());
-      }.bind(this)
-    });
-  },
   render: function() {
-    var danceClasses = this.state.data.map(function(rowJson) {
+    var danceClasses = this.props.data.map(function(rowJson) {
       return (
         <DanceClass date={rowJson.date} startTime={rowJson.startTime} endTime={rowJson.endTime} studio={rowJson.studio} instructor={rowJson.instructor} style={rowJson.style}/>
       );
@@ -181,14 +199,54 @@ level: "Bas"
 },
 ];
 
+var DanceClassesBody = React.createClass({
+  getInitialState: function() {
+    var initialDate = new Date().toISOString();
+    return {
+      date: initialDate,
+      data: []
+    };
+  },
+  onChildChanged: function(updatedDate) {
+    console.log("calling dance classes body on child changed...")
+    this.setState({ date: updatedDate }, this.loadClassesForDate);
+  },
+  loadClassesForDate: function() {
+    console.log("loading classes for date: " + this.state.date)
+    var formattedDate = formatDateForUrl(this.state.date);
+    var requestUrl = "http://take-prod.us-east-1.elasticbeanstalk.com/classes/" + formattedDate
+    $.ajax({
+      url: requestUrl,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  componentDidMount: function() {
+    this.loadClassesForDate();
+  },
+  render: function() {
+    return (
+      <div>
+        <ClassesNavBar initialDate={this.state.date} callbackParent={this.onChildChanged}/>
+        <Panel>
+          <DanceClassesTable initialDate={this.state.date} data={this.state.data} fill/>
+        </Panel>
+      </div>
+    );
+  }
+});
+
 const shebang = (
   <Container>
     <h1>Classes aggregator</h1>
     <p className="lead">Easily look through classes from various dance studios in NYC.</p>
-    {navBarInstance}
-    <Panel>
-      <DanceClassesTable url="http://take-prod.us-east-1.elasticbeanstalk.com/classes/2016-07-08" fill/>
-    </Panel>
+    <DanceClassesBody />
   </Container>
 );
 
